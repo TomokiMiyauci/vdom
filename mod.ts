516830f47c8caaf283633929a287d52a4ca94566
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-07-14 13:43:52 +0900

    feat(mod): export related modules

---
+++ mod.ts
@@ -0,0 +1,18 @@
+// Copyright © 2023 Tomoki Miyauchi. All rights reserved. MIT license.
+// This module is browser compatible.
+
+export {
+  type VAttr,
+  type VElement,
+  type VFragment,
+  type VNode,
+  VNodeType,
+  type VText,
+} from "./types.ts";
+export {
+  createAttr,
+  createDocumentFragment,
+  createElement,
+  createNode,
+  createText,
+} from "./dom.ts";
