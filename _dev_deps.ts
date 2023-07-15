b10d7b73f9ca2a4ac5777bec99253ff1c366ecfd
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-07-15 17:23:18 +0900

    chore: add dev deps

---
+++ _dev_deps.ts
@@ -0,0 +1,18 @@
+// Copyright 2023-latest Tomoki Miyauchi. All rights reserved. MIT license.
+
+export {
+  assert,
+  assertEquals,
+  assertFalse,
+  assertInstanceOf,
+  assertThrows,
+} from "https://deno.land/std@0.190.0/testing/asserts.ts";
+export {
+  beforeEach,
+  describe,
+  it,
+} from "https://deno.land/std@0.190.0/testing/bdd.ts";
+export {
+  assertSpyCalls,
+  spy,
+} from "https://deno.land/std@0.190.0/testing/mock.ts";
